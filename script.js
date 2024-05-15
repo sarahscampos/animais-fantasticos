5e2f5005b44dbd6dbe2a1a44ea35e2608c504cc8
sarah-campos <sarahstephanycampos@gmail.com>
2024-05-14 22:49:09 -0300

    navegacao por tabs

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
+//Para evitar erros, verifique se os elementos selecionados acima existem
+if(tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
+  
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+    tabContent[index].classList.add('ativo');
+  }
+  
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index); 
+    });
   })
-  tabContent[index].classList.add('ativo');
 }
 
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index); 
-  });
-})
