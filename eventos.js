351a2d97c51ac01573de039aeb278348f3a93afc
sarah-campos <sarahstephanycampos@gmail.com>
2024-05-16 01:04:56 -0300

    teste

---
+++ eventos.js
@@ -0,0 +1,22 @@
+// // const img = document.querySelector('img');
+
+// // function callback(event) {
+// //   console.log(event);
+// // }
+
+// // img.addEventListener('click', callback);
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   linksInternos.forEach((link) => {
+//     link.classList.remove('ativo');
+//   })
+//   event.currentTarget.classList.add('ativo');
+// }
+
+// const linksInternos = document.querySelectorAll('[href^="#"]');
+// console.log(linksInternos);
+
+// linksInternos.forEach((link) => {
+//     link.addEventListener('click', handleClick);
+// })
---
+++ selecao-elementos.js
@@ -0,0 +1,33 @@
+// const animais = document.getElementById('animais');
+// console.log(animais);
+
+// const primeiraLi = document.querySelector('li');
+// console.log(primeiraLi);
+
+// const linkInterno = document.querySelector('[href^="#"]');
+// console.log(linkInterno);
+
+// const animaisImg = document.querySelectorAll('.animais img');
+// console.log(animaisImg[1]);
+
+// const imagensTodas = document.querySelectorAll('img');
+// console.log(imagensTodas);
+
+// const imgs = document.querySelectorAll('img[src^="img/imagem"]');
+// console.log(imgs);
+
+// const linksInternos = document.querySelectorAll('[href^="#"]');
+// console.log(linksInternos);
+
+// const animaisDescricao = document.querySelector('.animais-descricao');
+// const primeiroH2 = animaisDescricao.querySelector('h2');
+// console.log(primeiroH2);
+
+// const h2Animais = document.querySelector('.animais-descricao h2');
+// console.log(h2Animais);
+
+
+// const paragrafos = document.querySelectorAll('p');
+// const ultimoParagrafo = paragrafos[paragrafos.length - 1];
+// console.log(ultimoParagrafo);
+
