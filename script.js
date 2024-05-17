f502b6280ee4e36879b0a19ab0dcde2aba9f46a6
sarah-campos <sarahstephanycampos@gmail.com>
2024-05-17 12:15:44 -0300

    scroll suave link interno

--- script.js
+++ script.js
@@ -42,4 +42,34 @@ function initAccordion() {
 
 
 initAccordion();
-//teste
+
+function initScrollSuave() {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  if(linksInternos.length) {
+    function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      
+      section.scrollIntoView({
+        behavior:'smooth',
+        block:'start',
+      });
+      
+      //forma alternativa
+      // const topo = section.offsetTop;
+      // window.scrollTo({
+      //   top: topo,
+      //   behavior: 'smooth',
+      // });
+    
+    }
+
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+    });
+  }
+}
+
+initScrollSuave();
