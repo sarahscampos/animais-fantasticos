1aef124269c0b853dce74d67a5675bf8391cfe23
sarah-campos <sarahstephanycampos@gmail.com>
2024-09-16 17:18:40 -0300

    refatorar accordion

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,28 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  if(accordionList.length) {
-
-    function activeAccordion() {
-      this.classList.toggle('ativo')
-      this.nextElementSibling.classList.toggle('ativo');
-    }
-    
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
+  }
+ 
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass)
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+  
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
     })
-  } 
+  }
+
+  init() {
+    if(this.accordionList.length) {
+      this.addAccordionEvent();
+    }
+  }
+
+  
+   
 }
 
 
