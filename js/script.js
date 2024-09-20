354dd0c7d35b7f33ff44fad2f1043d3e866bc44a
sarah-campos <sarahstephanycampos@gmail.com>
2024-09-20 19:59:56 -0300

    correcao caminho animais api

--- js/script.js
+++ js/script.js
@@ -35,5 +35,5 @@ const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
 
-fetchAnimais('animaisapi.json', '.numeros-grid');
+fetchAnimais('https://sarahscampos.github.io/animais-fantasticos/animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
