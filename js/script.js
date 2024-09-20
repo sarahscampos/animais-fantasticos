5ca1a1e57355783a841887cdbb79d0a9168b1269
sarah-campos <sarahstephanycampos@gmail.com>
2024-09-20 16:49:44 -0300

    refatorar scrollAnima

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import ScrollAnima from "./modules/scrollAnima.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
 import Modal from './modules/modal.js';
@@ -15,10 +15,12 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-initAnimacaoScroll();
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
+
 
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"', '[data-modal="container"');
 modal.init();
